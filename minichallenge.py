43ad5cc47e4ea30fa9ea5e170b48f9e28f9d48cf
Daniel Lopes <daniel.marqueslopes74@gmail.com>
2025-10-25 22:12:10 +0200

    minichallenge avec l'import de random, boucle while True, avec des try/excepts et des conditions if elif else

--- minichallenge.py
+++ minichallenge.py
@@ -2,8 +2,8 @@
 """
 Structure générale (sans code pour l’instant)
 
-Importer random
-Générer un nombre secret avec random.randint(1, 20)
+Importer random x
+Générer un nombre secret avec random.randint(1, 20) x
 Initialiser un compteur d’essais à 0
 Démarrer une boucle while :
 Demander une réponse à l’utilisateur
@@ -14,3 +14,25 @@ Si c’est le bon nombre → victoire
 Si c’est trop grand → message “plus petit”
 Si c’est trop petit → message “plus grand”
 """
+
+import random
+
+nombre_aleatoire=random.randint(1,20)
+
+compteur=0
+
+while True:
+    try:
+        devinette=int(input("Devinez le nombre de 1 à 20: "))
+        compteur+=1
+        if devinette == nombre_aleatoire:
+             print(f"Bravo! Vous avez trouvé le nombre en {compteur} essais")
+             break
+        elif devinette < nombre_aleatoire:
+            print("Le nombre choisi est trop petit! Vous êtes chaud ! Continuez....")
+        else:
+            print("Le nombre choisi est trop grand! Vous êtes chaud ! Continuez....")
+    except ValueError:
+        print("Erreur:Entrez un nombre valide s'il vous plait")
+
+
